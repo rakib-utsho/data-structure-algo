0d51b2eb7bb2ea402d8bd1abe8082b3efeae8ab6
rakib-utsho <rakibutsho1920@gmail.com>
2025-01-02 00:43:57 +0600

    alogo

---
+++ time complexity/program2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n1, n2, result;
+
+    n1 = 10; //assignment operation
+    n2 = 20; //assignment operation
+    result = n1+n2; //assignment operation, addition operation,
+
+    // time complexity is O(1) = Big O of 1
+
+    printf("Result is: %d", result);
+
+    return 0;
+}
